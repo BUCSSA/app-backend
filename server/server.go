a157b74b7346c38d5e1eadf7b71ada1b1ab075ee
ZhenjieRuan <zhenjier@bu.edu>
2016-05-23 16:00:17 -0400

    Included Godep. Added template server code

--- server/server.go
+++ server/server.go
@@ -7,3 +7,16 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 )
+
+func main() {
+	route := mux.NewRouter()
+	route.HandleFunc("/", HomeHandler)
+	n := negroni.Classic()
+	n.UseHandler(route)
+
+	http.ListenAndServe(":3000", n)
+}
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Hello Go Server!")
+}
